a6c801c90bba6a349bd5ff922e39d071499fcc9c
Peter Kim <peterkim.pk1@gmail.com>
2024-05-07 18:30:52 -0400

    Created function to calculate the percentage of correct guesses. Created function to signify that a round is over displaying the % of questions answered correct. Created tests to check for both functionalities

--- test/round-test.js
+++ test/round-test.js
@@ -52,7 +52,34 @@ describe ('takeTurn', () => {
         expect(guess).to.equal('Incorrect.')
     })
     it ('should give the correct feedback if the guess was correct', () => {
-        const secondGuess = takeTurn('wednesday', round)
-        expect (secondGuess).to.equal('Correct.')
+        const guess = takeTurn('42', round)
+        expect (guess).to.equal('Correct.')
     })
 })
+
+describe('calculatePercentCorrect, endRound', () => {
+    var firstCard, secondCard, thirdCard, deck, round
+    beforeEach(() => {
+        firstCard = createCard(1, "What is life", ['1','42','7','idk'], '42');
+        secondCard = createCard(2, "When will I finish this project?", ['wednesday','thursday','tuesday','never'],'wednesday');
+        thirdCard = createCard(4, "When will I be a wizard?", ['in 2 years','in 5 years','never','in 50 years'],'never');
+        deck = createDeck([firstCard,secondCard,thirdCard])
+        round = createRound(deck)
+    })
+    it('should calculate and return the percentage of correct guesses', () => {
+        takeTurn('42', round);
+        takeTurn('thursday', round);
+        takeTurn('in 50 years', round);
+        const correctPercentage = calculatePercentCorrect(round);
+        expect (correctPercentage).to.equal(33)
+    })
+    it('should print that the round is over when the user has gone through the whole deck', () => {
+        takeTurn('1', round);
+        takeTurn('thursday', round);
+        takeTurn('in 50 years', round);
+        const correctPercentage = calculatePercentCorrect(round);
+        const roundOverMessage = endRound(round);
+        expect (roundOverMessage).to.equal(`** Round over! ** You answered ${correctPercentage}% of the questions correctly!`)
+    })
+})
+
